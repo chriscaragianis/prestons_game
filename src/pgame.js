8e89df7c937280ad3f91cc70df502ba838f9912d
Chris Caragianis <chriscaragianis@gmail.com>
2015-09-07 20:48:40 -0400

    cleanup

--- src/pgame.js
+++ src/pgame.js
@@ -1,7 +1,4 @@
-
-
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
-
 
 function preload() {
   game.load.image('back', 'assets/backgroundTest.png');
@@ -10,22 +7,28 @@ function preload() {
 }
 
 function create() {
+  game.world.setBounds(0,0,5600,600);
+
   sky = game.add.tileSprite( 0, 0, 5600, 800, 'back');
   dirt = game.add.tileSprite(0, 500, 5600, 100, 'ground');
-  game.world.setBounds(0,0,5600,600);
   baddie = game.add.sprite(50, 400, 'badguy');
+
   game.physics.startSystem(Phaser.Physics.ARCADE);
+
   game.physics.arcade.enable(baddie);
-  game.physics.arcade.enable(dirt);
-  dirt.body.immovable= true;
-  game.camera.follow(baddie);
   baddie.body.collideWorldBounds = true;
   baddie.body.gravity.y = 300;
+
+  game.physics.arcade.enable(dirt);
+  dirt.body.immovable= true;
+
+  game.camera.follow(baddie);
 }
 
 function update() {
-  cursors = game.input.keyboard.createCursorKeys();
   game.physics.arcade.collide(baddie, dirt); 
+
+  cursors = game.input.keyboard.createCursorKeys();
   baddie.body.velocity.x = 0;
   if (cursors.right.isDown){
     baddie.body.velocity.x = 150;
@@ -36,6 +39,7 @@ function update() {
   if (cursors.up.isDown && baddie.body.touching.down){
     baddie.body.velocity.y = -150;
   }
+
   sky.tilePosition.x = -(game.camera.x * 0.22);
   dirt.tilePosition.x = -(game.camera.x * 0.52);
 }
